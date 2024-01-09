ada3968e23f6b02390b85a00e09ad4cc2e1b6961
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2024-01-10 02:51:37 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :entities
   resources :groups
   resources :users
@@ -9,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "home#index"
 end
